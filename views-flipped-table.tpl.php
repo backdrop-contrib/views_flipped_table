916f40cf2610edea86a14052d31737a52ce00348
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2012-05-04 12:46:36 +0100

    - #1561048 by joachim: Moved theme logic from the flipped table template to the preprocessor function.

--- views-flipped-table.tpl.php
+++ views-flipped-table.tpl.php
@@ -9,22 +9,19 @@
  * - $classes: A class or classes to apply to the table, based on settings.
  * - $row_classes: An array of classes to apply to each row, indexed by row
  *   number. This matches the index in $rows.
- * - $rows: An array of row items. Each row is an array of content.
+ * - $rows: The original array of row items. Each row is an array of content.
  *   $rows are keyed by row number, fields within rows are keyed by field ID.
+ * - $rows_flipped: An array of row items, with the original data flipped.
+ *   $rows_flipped are keyed by field name, each item within a row is keyed
+ *   by the original row number.
+ * - $row_classes_flipped: An array of classes to apply to each flipped row,
+ *   indexed by the field name.
  *   
  * @ingroup views_templates
  */
 ?>
 <?php
-  // Flip the table.
-  $row = array();
-  foreach ($rows as $col) {
-    foreach ($col as $ltr => $value) {
-      $row[$ltr][] = $value;
-    }
-  }
-  $first = isset($row['title']);
-  $element = 'odd';
+  $first = isset($rows_flipped['title']);
 ?>
 <table class="<?php print $classes; ?>">
   <?php if (!empty($title)) : ?>
@@ -33,42 +30,32 @@
 
   <?php if ($first) : ?>
     <thead>
-      <tr class="<?php echo $element; ?>">
+      <tr class="<?php print $row_classes_flipped['title']; ?>">
         <th>
         </th>
-        <?php foreach ($row['title'] as $title) : ?>
+        <?php foreach ($rows_flipped['title'] as $title) : ?>
           <th>
-          <?php echo $title; ?>
+          <?php print $title; ?>
           </th>
         <?php endforeach; ?>
       </tr>
     </thead>
   <?php  
-    $first = FALSE;
     endif; //$first
-    $element = 'even';
   ?>
   <tbody>
-    <?php foreach ($row as $field => $rowname) : ?>
-      <?php if ($field != 'title') : ?>
-        <tr class="<?php echo $element; ?>">
+    <?php foreach ($rows_flipped as $field_name => $row) : ?>
+      <?php if ($field_name != 'title') : ?>
+        <tr class="<?php print $row_classes_flipped[$field_name]; ?>">
           <th>
-            <?php echo $header[$field]; ?>
+            <?php echo $header[$field_name]; ?>
           </th>
-          <?php foreach ($rowname as $count => $item): ?>
+          <?php foreach ($row as $index => $item): ?>
             <td>
               <?php echo $item; ?>
             </td>
           <?php endforeach; ?>
         </tr>
-      <?php
-        if ($element == 'odd') {
-          $element = 'even';
-        }
-        else {
-          $element = 'odd';
-        }
-      ?>
       <?php endif; // field != title ?>
     <?php endforeach; ?>
   </tbody>
